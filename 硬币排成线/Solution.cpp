16ae48815ad3103585e179bfe75be9c6f3421bc3
zhongqian97 <994508794@qq.com>
2018-03-13 15:28:47 +0800

    硬币排成线

---
+++ 硬币排成线/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @return: A boolean which equals to true if the first player will win
+     */
+    bool firstWillWin(int n) {
+        // write your code here
+        return n%3;
+    }
+};
